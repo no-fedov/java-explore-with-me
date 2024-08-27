17d5cd6dad150f5021897a098b9dc1df9e271df0
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-08-28 01:45:41 +0300

    fix

--- main-service/src/main/java/ru/practicum/service/imp/EventAdminServiceImp.java
+++ main-service/src/main/java/ru/practicum/service/imp/EventAdminServiceImp.java
@@ -1,12 +1,54 @@
 package ru.practicum.service.imp;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.practicum.repository.EventRepository;
+import ru.practicum.dto.LocationDto;
+import ru.practicum.dto.event.EventDto;
+import ru.practicum.dto.event.URLParameterEventAdmin;
+import ru.practicum.model.QEvent;
+import ru.practicum.model.QLocation;
+import ru.practicum.model.status.StateEvent;
 import ru.practicum.service.EventAdminService;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventAdminServiceImp implements EventAdminService {
-    private final EventRepository eventRepository;
+    private final JPAQueryFactory queryFactory;
+    private final QEvent event = QEvent.event;
+    private final QLocation location = QLocation.location;
+
+    @Override
+    public List<EventDto> getEvents(URLParameterEventAdmin parameters) {
+        JPAQuery<EventDto> eventsQuery = queryFactory.select(Projections.constructor(EventDto.class,
+                        event.id,
+                        event.initiator.id,
+                        event.category.id,
+                        event.title,
+                        event.annotation,
+                        event.description,
+                        event.participantLimit,
+                        event.time,
+                        Projections.constructor(LocationDto.class, // Проекция на LocationDto
+                                location.locationId.lat,
+                                location.locationId.lon),
+                        event.paid,
+                        event.requestModeration,
+                        event.state,
+                        event.createdOn)
+                )
+                .from(event).leftJoin(event.location, location)
+                .where(event.initiator.id.in(parameters.getUsers()))
+                .where(event.state.in(parameters.getStates().stream().map(e -> StateEvent.valueOf(e)).toList()))
+                .where(event.category.id.in(parameters.getCategories()))
+                .where(event.time.goe(parameters.getRangeStart()))
+                .where(event.time.loe(parameters.getRangeEnd()))
+                .offset(parameters.getPage().getOffset())
+                .limit(parameters.getPage().getPageSize());
+        return eventsQuery.fetch();
+    }
 }
