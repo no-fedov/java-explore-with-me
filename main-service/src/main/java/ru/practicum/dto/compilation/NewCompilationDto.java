09410dd16764f5b32b82d3213ca59cbd3c844baf
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-09-01 01:08:30 +0300

    fix compilation logic

--- main-service/src/main/java/ru/practicum/dto/compilation/NewCompilationDto.java
+++ main-service/src/main/java/ru/practicum/dto/compilation/NewCompilationDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.dto.compilation;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -7,9 +8,9 @@ import java.util.List;
 
 @Data
 public class NewCompilationDto {
-    private String description;
     private List<Long> events = List.of();
     private Boolean pinned = false;
     @Size(min = 1, max = 50)
+    @NotBlank
     private String title;
 }
