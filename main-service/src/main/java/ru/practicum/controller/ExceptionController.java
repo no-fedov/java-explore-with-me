100c9015a796941b9bd55c6b518befe2b4f75bd7
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-09-03 18:58:56 +0300

    <fix> main-service: add log in ExceptionController

--- main-service/src/main/java/ru/practicum/controller/ExceptionController.java
+++ main-service/src/main/java/ru/practicum/controller/ExceptionController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.exception.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,20 +18,25 @@ import java.util.stream.Collectors;
 @RestControllerAdvice("ru.practicum")
 @Slf4j
 public class ExceptionController {
-
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleMissingURLParameter(MissingServletRequestParameterException e) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String,String> handleServerException(Exception e) {
+        writeLog(e);
         return Map.of("Ошибка: ", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMissingURLParameter(MissingServletRequestParameterException e) {
+        writeLog(e);
+        return Map.of("Ошибка: ", e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, List<String>> handleNoValidRequestBodyException(final MethodArgumentNotValidException e) {
         List<String> descriptionViolations = e.getFieldErrors().stream()
-                .map(x -> {
-                    return x.getField() + " -> " + x.getDefaultMessage();
-                })
+                .map(x -> x.getField() + " -> " + x.getDefaultMessage())
                 .collect(Collectors.toList());
         log.warn("Тело запроса содержит невалидные данные: {}.", descriptionViolations);
         return Map.of("Тело запроса содержит некорректные данные", descriptionViolations);
@@ -39,18 +45,21 @@ public class ExceptionController {
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFoundEntity(RuntimeException e) {
+        log.info("NOT FOUND EXCEPTION {}", e.getMessage());
         return Map.of("Ошибка: ", e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return Map.of("Ошибка: ", ex.getMessage());
+    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException e) {
+        writeLog(e);
+        return Map.of("Ошибка: ", e.getMessage());
     }
 
     @ExceptionHandler(NoValidParameter.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleNoValidParameter(RuntimeException e) {
+        writeLog(e);
         return Map.of("Ошибка: ", e.getMessage());
     }
 
@@ -58,6 +67,11 @@ public class ExceptionController {
             CategoryActionException.class, UserActionException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
     public Map<String, String> handleClientException(RuntimeException e) {
+        writeLog(e);
         return Map.of("Ошибка: ", e.getMessage());
+    }
+
+    private void writeLog(Throwable e) {
+        log.info("Exception: {}", Arrays.toString(e.getStackTrace()));
     }
 }
