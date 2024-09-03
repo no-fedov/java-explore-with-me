1c6bcb19d35021756e4818e9490c3c5abcbfdb28
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-09-03 20:46:45 +0300

    <fix> add @Validated on CompilationPublicController and add validation page size

--- main-service/src/main/java/ru/practicum/controller/publicapi/CompilationPublicController.java
+++ main-service/src/main/java/ru/practicum/controller/publicapi/CompilationPublicController.java
@@ -1,8 +1,11 @@
 package ru.practicum.controller.publicapi;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.controller.PageConstructor;
 import ru.practicum.dto.compilation.CompilationDto;
@@ -14,13 +17,14 @@ import java.util.List;
 @RequestMapping("/compilations")
 @AllArgsConstructor
 @Slf4j
+@Validated
 public class CompilationPublicController {
     private final CompilationServer compilationServer;
 
     @GetMapping
     public List<CompilationDto> getCompilations(@RequestParam(required = false) Boolean pinned,
-                                                @RequestParam(defaultValue = "0") Integer from,
-                                                @RequestParam(defaultValue = "10") Integer size) {
+                                                @PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
+                                                @Positive @RequestParam(defaultValue = "10") Integer size) {
         Pageable page = PageConstructor.getPage(from, size);
         return compilationServer.getCompilations(pinned, page);
     }
